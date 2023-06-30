7afbb37d322d4d47f0814ecc59a55e529d73f7fd
trinity <trinitydev001@gmail.com>
2023-06-30 14:56:55 +0100

    commit App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import '../src/CSS/home.css';
 import '../src/CSS/animatedSVG.css'
+import '../src/CSS/countries.css'
 import AnimatedSVG from './Components/AnimatedSVG';
+import Countries from './Components/Countries';
 
 import flutterwave from '../src/Images/flutterwave_svg.png';
 import org_logo from '../src/Images/org_logo.png';
@@ -140,8 +142,97 @@ function App() {
       </section>
     </main>
     <footer className='footer-sect'>
-      <div className="footer-nav flex-box">1</div>
-      <div className="footer-nav flex-social-handles">2</div>
+      <div className="footer-nav">
+        <div className="footer-flex-boxes">
+          <div className="footer-head">
+            <h5>Products</h5>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" className="ft__section__icon"><path d="M 6.087 10.998 C 5.507 10.458 5.507 9.541 6.085 9 L 6.135 8.954 C 6.71 8.416 7.603 8.415 8.18 8.951 L 11.66 12.184 C 11.852 12.362 12.148 12.363 12.34 12.185 L 15.856 8.932 C 16.413 8.416 17.283 8.446 17.803 9 L 17.803 9 C 18.324 9.554 18.302 10.424 17.755 10.952 L 12.682 15.843 C 12.299 16.211 11.696 16.217 11.307 15.855 Z"></path>
+            </svg>
+          </div>
+          <nav className='footer-nav-list'>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Collect Payments</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Send Money</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Store</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Payment Links</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Invoices</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Capital</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Grow</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Card Issuing</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>FaaS</a></div>
+          </nav>
+        </div>
+        <div className="footer-flex-boxes">
+          <div className="footer-head">
+            <h5>Resources</h5>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" className="ft__section__icon"><path d="M 6.087 10.998 C 5.507 10.458 5.507 9.541 6.085 9 L 6.135 8.954 C 6.71 8.416 7.603 8.415 8.18 8.951 L 11.66 12.184 C 11.852 12.362 12.148 12.363 12.34 12.185 L 15.856 8.932 C 16.413 8.416 17.283 8.446 17.803 9 L 17.803 9 C 18.324 9.554 18.302 10.424 17.755 10.952 L 12.682 15.843 C 12.299 16.211 11.696 16.217 11.307 15.855 Z"></path>
+            </svg>
+          </div>
+          <nav className='footer-nav-list'>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Pricing</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Support</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Blog</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Integrations</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Why you got charged</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Cookie settings</a></div>
+          </nav>
+        </div>
+        <div className="footer-flex-boxes">
+          <div className="footer-head">
+            <h5>Developers</h5>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" className="ft__section__icon"><path d="M 6.087 10.998 C 5.507 10.458 5.507 9.541 6.085 9 L 6.135 8.954 C 6.71 8.416 7.603 8.415 8.18 8.951 L 11.66 12.184 C 11.852 12.362 12.148 12.363 12.34 12.185 L 15.856 8.932 C 16.413 8.416 17.283 8.446 17.803 9 L 17.803 9 C 18.324 9.554 18.302 10.424 17.755 10.952 L 12.682 15.843 C 12.299 16.211 11.696 16.217 11.307 15.855 Z"></path>
+            </svg>
+          </div>
+          <nav className='footer-nav-list'>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>API Documentation</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>API Reference</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>API Status</a></div>
+          </nav>
+        </div>
+        <div className="footer-flex-boxes">
+          <div className="footer-head">
+            <h5>Flutterwave</h5>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" className="ft__section__icon"><path d="M 6.087 10.998 C 5.507 10.458 5.507 9.541 6.085 9 L 6.135 8.954 C 6.71 8.416 7.603 8.415 8.18 8.951 L 11.66 12.184 C 11.852 12.362 12.148 12.363 12.34 12.185 L 15.856 8.932 C 16.413 8.416 17.283 8.446 17.803 9 L 17.803 9 C 18.324 9.554 18.302 10.424 17.755 10.952 L 12.682 15.843 C 12.299 16.211 11.696 16.217 11.307 15.855 Z"></path>
+            </svg>
+          </div>
+          <nav className='footer-nav-list'>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Customers</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Careers</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Press kit</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Covid 19</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Barter</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Disha</a></div>
+            <div><a href="https://" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Send</a></div>
+          </nav>
+        </div>
+        <div className="footer-flex-boxes">
+          <div className="footer-head">
+            <h5>Contact</h5>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" className="ft__section__icon"><path d="M 6.087 10.998 C 5.507 10.458 5.507 9.541 6.085 9 L 6.135 8.954 C 6.71 8.416 7.603 8.415 8.18 8.951 L 11.66 12.184 C 11.852 12.362 12.148 12.363 12.34 12.185 L 15.856 8.932 C 16.413 8.416 17.283 8.446 17.803 9 L 17.803 9 C 18.324 9.554 18.302 10.424 17.755 10.952 L 12.682 15.843 C 12.299 16.211 11.696 16.217 11.307 15.855 Z"></path>
+            </svg>
+          </div>
+          <nav className='footer-nav-list'>
+            <div><a href="mailto:hi@flutterwavego.com" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>hi@flutterwavego.com</a></div>
+            <div><a href="tel:+23418889595" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>+234 1-8889595</a></div>
+            <div><a href="tel:070035888379283" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>0700-FLUTTERWAVE</a></div>
+            <div><a href="https://twitter.com/flwsupport" target="_blank" rel="noopener noreferrer" className='footer-ALinks'>Twitter Support</a></div>
+            <div>
+              <p className="footer-text">
+                Call lines are open 08:00 to 17:00 WAT, Mondays - Fridays
+              </p>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <div className="footer-nav flex-social-handles">
+        <Countries></Countries>
+        <nav className='social__links'>
+          <span><a href="https://twitter.com/thflutterwave">Twitter</a></span>
+          <span><a href="https://www.facebook.com/theFlutterwave/">Facebook</a></span>
+          <span><a href="https://www.instagram.com/theflutterwave/">Instagram</a></span>
+          <span><a href="https://www.youtube.com/channel/UCBIAbIvsVyppFIiQP9CV-zQ">YouTube</a></span>
+          <span><a href="https://www.linkedin.com/company/flutterwave/">LinkedIn</a></span>
+        </nav>
+      </div>
       <div className="footer-nav flex-terms-policy">3</div>
     </footer>
     </React.Fragment>
